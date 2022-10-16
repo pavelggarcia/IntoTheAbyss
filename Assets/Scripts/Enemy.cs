ea0af60390cb65e358f809b388ac31e04699d25e
Pavel G Garcia <pavel.g.garcia@outlook.com>
2022-10-16 17:32:45 -0400

    added enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private int _enemySpeed = 4;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down * Time.deltaTime * _enemySpeed);
+        if (transform.position.y <= -6)
+        {
+            float randomX = Random.Range(-12.0f, 12.0f);
+            transform.position = new Vector3(randomX, 8, 0);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log("Hit :" + other.transform.name);
     }
 }
