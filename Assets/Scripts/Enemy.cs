fcc82af4663e66fc4040d6505769865997a4d5ed
Pavel G Garcia <pavel.g.garcia@outlook.com>
2022-11-17 19:02:58 -0500

    added enemy explosions

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,32 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _enemySpeed = 4;
     private Player _player;
+    private Animator _anim;
+    private Rigidbody2D _rigidBody2D;
+    private BoxCollider2D _boxCollider2D;
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        if(_player == null)
+        {
+            Debug.LogError("Player is NULL");
+        }
+        _anim = GetComponent<Animator>();
+        if(_anim == null)
+        {
+            Debug.LogError("Animator is NULL");
+        }
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        if(_rigidBody2D == null)
+        {
+            Debug.LogError("RigidBody is NULL");
+        }
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        if(_boxCollider2D == null)
+        {
+            Debug.LogError("Box Collider is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -34,7 +56,10 @@ public class Enemy : MonoBehaviour
             {
                 player.Damage();
             }
-            Destroy(this.gameObject);
+            RemoveComponents();
+            _anim.SetTrigger("OnEnemyDeath");
+            
+            Destroy(this.gameObject, 2.5f);
         }
 
 
@@ -45,8 +70,16 @@ public class Enemy : MonoBehaviour
             {
                 _player.AddToScore(10);
             }
+            RemoveComponents();
+            _anim.SetTrigger("OnEnemyDeath");
             
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, 2.5f);
         }
     }
+    private void RemoveComponents()
+    {
+        Destroy(_rigidBody2D);
+        Destroy(_boxCollider2D);
+    }
+    
 }
